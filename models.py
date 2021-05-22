9d04bda86d0ff41f08e19f9ed527176cbdf50603
Irfan <ibunjaku@student.ethz.ch>
2021-05-22 11:28:24 +0200

    added zero velocity implementation

--- models.py
+++ models.py
@@ -13,7 +13,8 @@ from losses import mse
 def create_model(config):
     # This is a helper function that can be useful if you have several model definitions that you want to
     # choose from via the command line. For now, we just return the Dummy model.
-    return DummyModel(config)
+    #return DummyModel(config)
+    return ZeroVelocity(config)
 
 
 class BaseModel(nn.Module):
@@ -92,5 +93,59 @@ class DummyModel(BaseModel):
             # We only want to do backpropagation in training mode, as this function might also be called when evaluating
             # the model on the validation set.
             total_loss.backward()
+
+        return loss_vals, targets
+
+
+class ZeroVelocity(BaseModel):
+    """
+    Implementation of the zero velocity model for a comparison
+    """
+
+    def __init__(self, config):
+        self.n_history =1
+        super(ZeroVelocity, self).__init__(config)
+
+    # noinspection PyAttributeOutsideInit
+    def create_model(self):
+        # In this model we simply feed the last time steps of the seed to a dense layer and
+        # predict the targets directly.
+        self.dense = nn.Linear(in_features=self.n_history * self.pose_size,
+                               out_features=self.config.target_seq_len * self.pose_size)
+
+    def forward(self, batch: AMASSBatch):
+        """
+        The forward pass.
+        :param batch: Current batch of data.
+        :return: Each forward pass must return a dictionary with keys {'seed', 'predictions'}.
+        """
+        model_out = {'seed': batch.poses[:, :self.config.seed_seq_len],
+                     'predictions': None}
+        batch_size = batch.batch_size
+        model_in = batch.poses[:, self.config.seed_seq_len-self.n_history:self.config.seed_seq_len]
+        pred = model_in.repeat_interleave(self.config.target_seq_len, dim = 0)
+        #pred = self.dense(model_in.reshape(batch_size, -1))
+        model_out['predictions'] = pred.reshape(batch_size, self.config.target_seq_len, -1)
+        return model_out
+
+    def backward(self, batch: AMASSBatch, model_out):
+        """
+        The backward pass.
+        :param batch: The same batch of data that was passed into the forward pass.
+        :param model_out: Whatever the forward pass returned.
+        :return: The loss values for book-keeping, as well as the targets for convenience.
+        """
+        predictions = model_out['predictions']
+        targets = batch.poses[:, self.config.seed_seq_len:]
+
+        total_loss = mse(predictions, targets)
+
+        # If you have more than just one loss, just add them to this dict and they will automatically be logged.
+        loss_vals = {'total_loss': total_loss.cpu().item()}
+
+        #if self.training:
+            # We only want to do backpropagation in training mode, as this function might also be called when evaluating
+            # the model on the validation set.
+        #    total_loss.backward()
 
         return loss_vals, targets
