2e12fbab6915fbfb4e19889efacf572a025d7f0c
Felix Sarnthein <safelix@student.ethz.ch>
2021-06-05 13:22:23 +0200

    update per joint loss

--- losses.py
+++ losses.py
@@ -37,12 +37,12 @@ def loss_pose_all_mean(predictions, targets):
     diff = predictions - targets
     return (diff * diff).mean()
 
-def loss_pose_joint_sum(predictions, targets):
+def loss_pose_joint_sum(predictions, targets, n_frames=144):
     """
     Loss computed as described https://github.com/eth-ait/spl/blob/6b37cc0a61c69b6e43187800d6589eb9cfaa9799/spl/model/base_model.py
     """
     diff = predictions - targets
-    per_joint_loss = (diff*diff).view(-1, 143, 15, 9)
+    per_joint_loss = (diff*diff).view(-1, n_frames, 15, 9)
     per_joint_loss = per_joint_loss.sum(dim=-1)
     per_joint_loss = per_joint_loss.sqrt()
     per_joint_loss = per_joint_loss.sum(dim=-1)
