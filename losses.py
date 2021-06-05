8366007ff60cc61dbf0fb0b2345e84868b9e12bc
Laure Ciernik <laure.ciernik@gmail.com>
2021-06-05 22:45:10 +0200

    added per joint loss

--- losses.py
+++ losses.py
@@ -28,3 +28,15 @@ def rmse(predictions, targets):
     loss_per_sample_and_seq = (diff * diff).sum(dim=-1)  # (N, F)
     loss_per_sample_and_seq = loss_per_sample_and_seq.sqrt()
     return loss_per_sample_and_seq.mean()
+
+def loss_pose_joint_sum(predictions, targets):
+    """
+    Loss computed as described https://github.com/eth-ait/spl/blob/6b37cc0a61c69b6e43187800d6589eb9cfaa9799/spl/model/base_model.py
+    """
+    diff = predictions - targets
+    per_joint_loss = (diff * diff).view(-1, 143, 15, 9)
+    per_joint_loss = per_joint_loss.sum(dim=-1)
+    per_joint_loss = per_joint_loss.sqrt()
+    per_joint_loss = per_joint_loss.sum(dim=-1)
+
+    return per_joint_loss.mean()
