9b4348d377822d7a646fa0ec2ef9f671728ae47e
Laure Ciernik <laure.ciernik@gmail.com>
2021-06-06 12:32:02 +0200

    change dim in per joint loss

--- losses.py
+++ losses.py
@@ -29,12 +29,12 @@ def rmse(predictions, targets):
     loss_per_sample_and_seq = loss_per_sample_and_seq.sqrt()
     return loss_per_sample_and_seq.mean()
 
-def loss_pose_joint_sum(predictions, targets):
+def loss_pose_joint_sum(predictions, targets, n_frames = 144):
     """
     Loss computed as described https://github.com/eth-ait/spl/blob/6b37cc0a61c69b6e43187800d6589eb9cfaa9799/spl/model/base_model.py
     """
     diff = predictions - targets
-    per_joint_loss = (diff * diff).view(-1, 143, 15, 9)
+    per_joint_loss = (diff * diff).view(-1, n_frames, 15, 9)
     per_joint_loss = per_joint_loss.sum(dim=-1)
     per_joint_loss = per_joint_loss.sqrt()
     per_joint_loss = per_joint_loss.sum(dim=-1)
