4b05d110b81825ab8deb21045f2dca4662a0a604
Laure Ciernik <laure.ciernik@gmail.com>
2021-06-08 09:49:32 +0200

    added max_norm flag to condif

--- train.py
+++ train.py
@@ -216,7 +216,7 @@ def main(config):
 
             # Update params.
             if config.clip_gradient:
-                torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=1)
+                torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=config.max_norm)
 
             optimizer.step()
 
